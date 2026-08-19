6cf83795fc27800ed08b6c7caa8a0cf33f0bc55e
Gunnar Smith <gunnar@wustl.edu>
2026-08-19 13:42:57 -0500

    replace static filepaths with use of cwd

--- repos/spack_repo/washu_it_ris/ris/packages/dmtcp/package.py
+++ repos/spack_repo/washu_it_ris/ris/packages/dmtcp/package.py
@@ -3,6 +3,8 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+import os
+
 from spack.package import *
 
 
@@ -12,12 +14,16 @@ class Dmtcp(Package):
     with no modifications to user code or to the O/S."""
 
     homepage = "https://dmtcp.sourceforge.net/"
-    url = "file:////storage2/fs1/RIS-Services/Active/compute2-public/spack/mirrors/dmtcp/dmtcp-4.0.0-x8664.tgz"
+    #url = "file:////storage2/fs1/RIS-Services/Active/compute2-public/spack/mirrors/dmtcp/dmtcp-4.0.0-x8664.tgz"
+    manual_download = True
 
     license("LGPL-3.0-only")
 
     maintainers("jcaroline")
-    version("4.0.0", sha256="eeb09d51891ae2dc7b625ce36c55a77a7b672ac6987c104e70cdb82782339e9e", url = "file:////storage2/fs1/RIS-Services/Active/compute2-public/spack/mirrors/dmtcp/dmtcp-4.0.0-x8664.tgz")
+    version("4.0.0", sha256="eeb09d51891ae2dc7b625ce36c55a77a7b672ac6987c104e70cdb82782339e9e")
+
+    def url_for_version(self, version):
+        return "file://{0}/dmtcp-{1}-x8664.tgz".format(os.getcwd,version)
     
     def install(self, spec, prefix):
         # This manually copies your pre-built files into the Spack prefix
